6b848335a1075dd765e28fbeea91e0aa0c1ff1fa
Tijana Ognjenovic <ognjenovic.tijana1912@gmail.com>
2020-01-29 22:55:49 +0100

    final setup

---
+++ todo-frontend/src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+Vue.use(Vuex);
+// Vuex je centralizovano skladiste state-a nase aplikacije, kao npr. Redux
+// U ovom primeru imamo samo baseUrl, koji setujemo kako bismo iz cele aplikacije
+// znali na kojoj adresi nam je backend server
+export default new Vuex.Store({
+  state: {
+    baseUrl: 'http://localhost:8080/itehUt/todo-frontend'
+  },
+  mutations: {},
+  actions: {},
+  modules: {}
+});
